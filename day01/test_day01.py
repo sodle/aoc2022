423b25e12d37773e5c4b48c5dec5ef873504ff9d
Scott Odle <scott@sjodle.com>
2022-11-30 22:48:57 -0700

    Day 1 + input parsing

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -1,2 +1,17 @@
-def test_main():
-    pass
+from day01.__main__ import part1, part2
+
+test_input = [
+    [1000, 2000, 3000],
+    [4000],
+    [5000, 6000],
+    [7000, 8000, 9000],
+    [10000],
+]
+
+
+def test_part1():
+    assert part1(test_input) == 24000
+
+
+def test_part2():
+    assert part2(test_input) == 45000
