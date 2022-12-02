145a203752b8e343ec7ca47928b4ea8492c39bda
Scott Odle <scott@sjodle.com>
2022-12-01 22:47:07 -0700

    Day 2 solved

--- day02/__main__.py
+++ day02/__main__.py
@@ -1,5 +1,24 @@
+# Day 02: Rock Paper Scissors
+from pathlib import Path
+
+from typing import List
+
+from shared.rps import parse, Match
+
+
+def part1(matches: List[Match]) -> int:
+    return sum([match.score() for match in matches])
+
+
+def part2(matches: List[Match]) -> int:
+    return sum([match.throw() for match in matches])
+
+
 def main():
-    print("yeet!")
+    input_path = Path(__file__).parent.joinpath("input.txt")
+    matches = parse(input_path)
+    print(f"Part 1:\t{part1(matches)}")
+    print(f"Part 2:\t{part2(matches)}")
 
 
 if __name__ == "__main__":
