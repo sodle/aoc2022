2de501837bc2ab5e4c1e787f913f263432f9069b
Scott Odle <scott@sjodle.com>
2022-12-13 20:41:55 -0800

    day 13 part 1, passing tests but not real input

---
+++ day13/__main__.py
@@ -0,0 +1,77 @@
+# Day 13: Distress Signal
+from pathlib import Path
+import json
+
+from typing import Union, Optional
+
+
+def compare_packets(left: list[Union[int, list]], right: list[Union[int, list]],
+                    is_recursive: bool = False) -> Optional[bool]:
+    # if left is empty but right isn't, we're definitely in order
+    if len(left) == 0 and len(right) > 0:
+        return True
+
+    for index, left_value in enumerate(left):
+        if index >= len(right):
+            return False
+
+        right_value = right[index]
+
+        # both are ints, so just compare them
+        if isinstance(left_value, int) and isinstance(right_value, int):
+            if left_value != right_value:
+                # if the values are different, compare them and decide if we're in order, otherwise keep going
+                return left_value < right_value
+            else:
+                continue
+
+        # at least one is list, so make sure they both are, and recurse
+        if not isinstance(left_value, list):
+            left_value = [left_value]
+        if not isinstance(right_value, list):
+            right_value = [right_value]
+
+        if isinstance(left_value, list) and isinstance(right_value, list):
+            compare = compare_packets(left_value, right_value, is_recursive=True)
+            if compare is not None:
+                return compare
+
+    # if none of the previous checks fail, and we're in a recursive call, return inconclusive.
+    # Otherwise, we're in order!
+    if is_recursive:
+        return None
+    else:
+        return True
+
+
+def part1(lines: list[str]) -> int:
+    ordered_indices = []
+    index = 1
+
+    pair = []
+    for line in lines:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+
+        pair.append(json.loads(line))
+        if len(pair) == 2:
+            if compare_packets(*pair):
+                ordered_indices.append(index)
+
+            pair = []
+            index += 1
+
+    return sum(ordered_indices)
+
+
+def part2(lines: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    input_path = Path(__file__).parent.joinpath("input.txt")
+    with input_path.open() as input_file:
+        input_lines = input_file.readlines()
+        print(f"Part 1:\t{part1(input_lines)}")
+        print(f"Part 2:\t{part2(input_lines)}")
---
+++ day13/test_day13.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from day13.__main__ import part1, part2
+
+
+def test_part1():
+    with Path(__file__).parent.joinpath("test_input").joinpath("packets.txt").open() as input_file:
+        assert part1(input_file.readlines()) == 13
+
+
+def test_part2():
+    with Path(__file__).parent.joinpath("test_input").joinpath("packets.txt").open() as input_file:
+        assert part2(input_file.readlines()) == 0
