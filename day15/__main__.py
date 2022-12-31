439309e41d78ae6efd1df2fcd4b37bd8012a67d1
Scott Odle <scott@sjodle.com>
2022-12-31 14:35:22 -0800

    day 15 part 2

--- day15/__main__.py
+++ day15/__main__.py
@@ -1,6 +1,8 @@
 # Day 15: Beacon Exclusion Zone
 from pathlib import Path
 import re
+import random
+import threading
 
 scan_regex = re.compile(r'Sensor at x=(?P<sensor_x>-?\d+), y=(?P<sensor_y>-?\d+): '
                         r'closest beacon is at x=(?P<beacon_x>-?\d+), y=(?P<beacon_y>-?\d+)')
@@ -35,53 +37,41 @@ def part1(lines: list[str], check_row: int = 2000000) -> int:
 
 
 def part2(lines: list[str], bound: int = 4000000) -> int:
-    exclusions: list[set[int]] = [set() for _ in range(bound + 1)]
-
-    bounding_set = set(range(bound + 1))
+    sensors = []
     for line in lines:
         line = line.strip()
-        if len(line) > 0:
-            sensor_x, sensor_y, beacon_x, beacon_y = scan_regex.fullmatch(line).groups()
-
-            sensor_x = int(sensor_x)
-            sensor_y = int(sensor_y)
-            beacon_x = int(beacon_x)
-            beacon_y = int(beacon_y)
-
-            if beacon_x in range(bound + 1) and beacon_y in range(bound + 1):
-                exclusions[beacon_y].add(beacon_x)
-
+        match = scan_regex.fullmatch(line)
+        if match is not None:
+            sensor_x, sensor_y, beacon_x, beacon_y = [int(g) for g in match.groups()]
             sensor_range = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
+            sensors.append((sensor_x, sensor_y, sensor_range))
 
-            columns = list(range(sensor_x - sensor_range, sensor_x + sensor_range + 1))
-            for row in range(sensor_range + 1):
+    for sensor_x, sensor_y, sensor_range in sensors:
+        scan_radius = sensor_range + 1
+        scan_x = scan_radius
+        scan_y = 0
 
-                up = sensor_y - row
-                down = sensor_y + row
+        while scan_x >= scan_y:
+            for check_x, check_y in [
+                (sensor_x + scan_x, sensor_y + scan_y),
+                (sensor_x - scan_x, sensor_y + scan_y),
+                (sensor_x + scan_x, sensor_y - scan_y),
+                (sensor_x - scan_x, sensor_y - scan_y),
+            ]:
+                if 0 <= check_x <= bound and 0 <= check_y <= bound:
+                    for other_x, other_y, other_range in sensors:
+                        if abs(check_x - other_x) + abs(check_y - other_y) <= other_range:
+                            break
+                    else:
+                        return check_x * 4000000 + check_y
 
-                column_set = set(columns).intersection(bounding_set)
-
-                if up in bounding_set:
-                    exclusions[up] = exclusions[up].union(column_set)
-                if down in bounding_set:
-                    exclusions[down] = exclusions[down].union(column_set)
-
-                columns.pop()
-
-                if len(columns) > 0:
-                    columns.pop(0)
-
-    for y, row in enumerate(exclusions):
-        if len(row) == bound:
-            x = (bounding_set - row).pop()
-            return x * 4000000 + y
-
-    return 0
+            scan_x -= 1
+            scan_y += 1
 
 
 if __name__ == "__main__":
-    input_path = Path(__file__).parent.joinpath("test_input").joinpath("test_input.txt")
+    input_path = Path(__file__).parent.joinpath("input.txt")
     with input_path.open() as input_file:
         input_lines = input_file.readlines()
-        # print(f"Part 1:\t{part1(input_lines, check_row=10)}")
-        print(f"Part 2:\t{part2(input_lines, bound=20)}")
+        print(f"Part 1:\t{part1(input_lines)}")
+        print(f"Part 2:\t{part2(input_lines)}")
